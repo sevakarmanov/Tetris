3e3ff511a9a3802cf4c822dc2a66f6b01e79a9ac
Vsevolod <sevakarmanov@yandex.ru>
2025-05-18 18:39:15 +0300

    Начало разработки, реализованы классы Point, Square и Stick

---
+++ Tetris/Tetris/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char c;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+
+        public Point(int a, int b, char sym)
+        {
+            x = a;
+            y = b;
+            c = sym;
+        }
+
+        public Point() { }
+    }
+}
---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            Square s = new Square(2, 5, '*');
+            s.Draw();
+
+            Stick st = new Stick(4, 8, '#');
+            st.Draw();
+
+            //Point p1 = new Point(2, 3, '*');
+            //p1.Draw();
+
+            //Point p2 = new Point()
+            //{
+            //    x = 4,
+            //    y = 5,
+            //    c = '*'
+            //};
+            //p2.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
+
